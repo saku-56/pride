de5ca0bc7fce044523aa24f613d193345fa5902d
saku-56 <icecat3956@gmail.com>
2025-08-18 20:01:37 +0900

    Add: ハンドルネームの追加
    ハンドルネームを新規作成で入力、投稿一覧で表示できるように実装しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:name, :content)
   end
 end
